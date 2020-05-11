18c3c3bbcfa67cc7c6a3dc2049bd26df6eeb5114
AlvinMi <alvin.mi0620@gmail.com>
2020-05-11 23:54:57 +0800

    view/blog

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,53 @@ const HomeComponent = loadable(() =>
 const AboutComponent = loadable(() =>
   import(/* webpackChunkName: "about" */ "./views/About")
 );
+const BlogComponent = loadable(() =>
+  import(/* webpackChunkName: "blog" */ "./views/Blog")
+);
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <h1>Hi I'm YuHui</h1>
-        <Router>
-          <ul>
-            <li>
-              <Link to="/">To Home</Link>
-            </li>
-            <li>
-              <Link to="/about">To About</Link>
-            </li>
-          </ul>
-          <Route exact path="/" component={HomeComponent}></Route>
-          <Route path="/about" component={AboutComponent}></Route>
-        </Router>
+        <div className="content">
+          <div className="slice-group">
+            <h1>Hi I'm Yu Hui</h1>
+            <p>
+              <strong>Front End Development</strong> | Technical Writer
+            </p>
+            <Router>
+              <ul className="group-list">
+                <li>
+                  <a href="https://github.com/alvinmi">GITHUB</a>
+                </li>
+
+                <li>
+                  <Link to="/blog">BLOG</Link>
+                </li>
+                <li>
+                  <a href="https://www.instagram.com/alvinmi0620/">INSTAGRAM</a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/mr_yuhui">TWITTER</a>
+                </li>
+                <li>
+                  <a href="https://weibo.com/u/5703864435/">Weibo</a>
+                </li>
+                <li>
+                  <a href="mailto:alvin.mi0620@gmail.com">EMAIL</a>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/">To Home</Link>
+                </li>
+              </ul>
+              {/* <Route exact path="/" component={HomeComponent}></Route> */}
+              {/* <Route path="/about" component={AboutComponent}></Route> */}
+            </Router>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/views/Blog/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class Blog extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Blog</p>
+      </div>
+    );
+  }
+}
+
+export default Blog;
